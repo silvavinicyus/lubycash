f2c779b7899f3b88caed39a45064509901268f47
Vinicyus <vinicyus346@gmail.com>
2022-02-16 17:23:11 -0300

    feat: trying to make create user work with the MS

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Database from '@ioc:Adonis/Lucid/Database';
 import Address from 'App/Models/Address';
+import Permission from 'App/Models/Permission';
 import User from 'App/Models/User';
+import UserPermission from 'App/Models/UserPermission';
 import { Kafka } from 'kafkajs';
 
 export default class UsersController {
@@ -20,9 +22,11 @@ export default class UsersController {
       phone,
       cpf_number: cpfNumber,
       average_salary: averageSalary,
+      city: city,
+      state: state,
+      zipCode: zipCode,
+      password: password,
     };
-
-    console.log(message);
 
     const producerNewUser = kafka.producer();
 
@@ -33,32 +37,50 @@ export default class UsersController {
       messages: [{ value: JSON.stringify(message) }],
     });
 
-    // await Database.transaction(async (trx) => {
-    //   const address = new Address();
-
-    //   address.city = city;
-    //   address.state = state;
-    //   address.zipCode = zipCode;
-
-    //   address.useTransaction(trx);
-    //   await address.save();
-
-    //   const user = new User();
-
-    //   user.email = email;
-    //   user.password = password;
-    //   user.addressId = address.id;
-
-    //   user.useTransaction(trx);
-    //   await user.save();
-
-    //   return user;
-    // });
-
     producerNewUser.disconnect();
 
-    return {
-      message: 'okay',
-    };
+    const consumerUserCreated = kafka.consumer({ groupId: 'user_created' });
+    await consumerUserCreated.connect();
+    await consumerUserCreated.subscribe({ topic: 'usercreated', fromBeginning: false });
+
+    await consumerUserCreated.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const data = message.value!.toString();
+        const dataJson = JSON.parse(data);
+
+        // await Database.transaction(async (trx) => {
+        const address = new Address();
+
+        address.city = city;
+        address.state = state;
+        address.zipCode = zipCode;
+
+        // address.useTransaction(trx);
+        await address.save();
+
+        const user = new User();
+
+        user.email = email;
+        user.password = password;
+        user.addressId = address.id;
+
+        // user.useTransaction(trx);
+        await user.save();
+
+        const userPermission = new UserPermission();
+        const permission = await Permission.findByOrFail('type', 'client');
+
+        userPermission.userId = user.id;
+        userPermission.permissionId = permission.id;
+
+        // userPermission.useTransaction(trx);
+        await userPermission.save();
+        // });
+      },
+    });
+
+    return response.ok({
+      message: 'Your request has been received, we will send you an email when we have a result!',
+    });
   }
 }
